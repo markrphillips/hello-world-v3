399070c58c0a57d5d8fee8045753f5a8088fabde
markrphillips <markrphillips1@gmail.com>
2018-05-10 21:17:18 -0400

    Add files via upload
    New file added.

---
+++ MyTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author mark
+ *
+ */
+public class MyTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is MyTest running");
+		
+	}
+
+}
